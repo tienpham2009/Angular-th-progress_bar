70e2ff22dd8d9928c6a55a70e70ace42e5eedb1e
tienpham2009 <tiendx2009@gmail.com>
2021-07-15 14:55:10 +0700

    progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgessBarComponent } from './progress-bar/progess-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgessBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/progress-bar/progess-bar.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-progess-bar',
+  templateUrl: './progess-bar.component.html',
+  styleUrls: ['./progess-bar.component.css'],
+})
+export class ProgessBarComponent implements OnInit {
+  @Input() backgroundColor = '#D9D9D9';
+  @Input() progressColor = '#4CAF50';
+  @Input() progress = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loading();
+  }
+
+  loading() {
+    let interval = setInterval(() => {
+      this.progress = +this.progress + 5;
+      if (this.progress == 100) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+}
